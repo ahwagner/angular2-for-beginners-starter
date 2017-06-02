eb6f6dfdca475d654d1c501658e5380177fd356a
Alex H. Wagner, PhD <awagner24@wustl.edu>
2017-06-02 17:22:44 -0500

    component composition

--- app/app.ts
+++ app/app.ts
@@ -2,17 +2,18 @@
 import {Component, NgModule} from "@angular/core";
 import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
 import {BrowserModule} from "@angular/platform-browser";
+import {SearchBox} from "./search-box/search-box.component";
 
 @Component({
     selector: 'app',
-    template: `Hello Ng2 World !`
+    template: `<search-box></search-box>`
 })
 export class App {
 
 }
 
 @NgModule({
-    declarations: [App],
+    declarations: [App, SearchBox],
     imports: [BrowserModule],
     bootstrap: [App]
 })
