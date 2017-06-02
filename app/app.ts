8b2595eed2625f540e471b2fd6883b00cef12985
Alex H. Wagner, PhD <awagner24@wustl.edu>
2017-06-02 17:04:27 -0500

    bootstrap app

---
+++ app/app.ts
@@ -0,0 +1,23 @@
+
+import {Component, NgModule} from "@angular/core";
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
+import {BrowserModule} from "@angular/platform-browser";
+
+@Component({
+    selector: 'app',
+    template: `Hello Ng2 World !`
+})
+export class App {
+
+}
+
+@NgModule({
+    declarations: [App],
+    imports: [BrowserModule],
+    bootstrap: [App]
+})
+export class AppModule{
+
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
