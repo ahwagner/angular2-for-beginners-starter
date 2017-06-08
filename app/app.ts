1597d0a70a945ea78b162d433ed761b6efc03c17
Alex H. Wagner, PhD <awagner24@wustl.edu>
2017-06-08 10:31:58 -0500

    update source files

--- app/app.ts
+++ app/app.ts
@@ -1,28 +1,40 @@
 
-import {Component, NgModule} from "@angular/core";
+import {Component} from "@angular/core";
+import {NgModule} from "@angular/core";
 import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
 import {BrowserModule} from "@angular/platform-browser";
-import {ColorSample} from "./color-sample/color-sample";
+
+import {CollapseOnClick} from "./collapse-on-click.directive";
+
 
 
 @Component({
-    selector: 'app',
+    selector:'app',
     template: `
-        <color-sample #colorsample></color-sample>
-        <input #colorbox placeholder="Type Your Color Here" (keyup)="colorsample.background = colorbox.value">
         
-    `
+        <div collapse-on-click #panel="cp" 
+                            class="card card-strong disable-text-selection">
+           
+            <i class="md-icon collapsible-indicator" *ngIf="!panel.collapsed">arrow_drop_down</i>
+            <i class="md-icon collapsible-indicator" *ngIf="panel.collapsed">arrow_drop_up</i>
+           
+            <div class="collapsible-section" *ngIf="!panel.collapsed">   
+                This page section is collapsible, double click it and it will collapse or expand.
+            </div>
+        </div>
+        `
 })
 export class App {
+
 
 }
 
 @NgModule({
-    declarations: [App, ColorSample],
+    declarations: [App, CollapseOnClick],
     imports: [BrowserModule],
     bootstrap: [App]
 })
-export class AppModule{
+export class AppModule {
 
 }
 
